d5df8a1a6c68ea1824a50794d426bed0b06e25e4
Aditya Cholkar <adityacholkar@gmail.com>
2025-04-29 16:03:10 +0530

    Refactor database config

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,12 +2,13 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '5432', 10),
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
-  database: process.env.DB_NAME || 'job_matching',
+  url:
+    process.env.DATABASE_URL ||
+    'postgresql://postgres:postgres@db.bxoqdcntcejhcriwzzdt.supabase.co:5432/postgres',
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: false, // Set to false in production
+  ssl: {
+    rejectUnauthorized: false, // Needed for connecting to Supabase from Railway
+  },
   logging: true,
 };
